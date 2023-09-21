2a31b05020dbac8864ddeaccfea353701124b8e6
Sekiiigan <vidal.kerian@lyceejulesfil.fr>
2023-09-21 10:27:30 +0200

    Commit ex2

---
+++ EX2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; //Ceciest un commentaire sur une ligne
+
+int main ()
+/*Ceci est un commentaire
+sur plusieurs lignes*/
+{
+    cout << "Hello world" << endl;
+    return 0;
+}
