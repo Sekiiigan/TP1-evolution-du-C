28497504e047f219d697c9b81030bddf78d38c8f
Sekiiigan <vidal.kerian@lyceejulesfil.fr>
2023-09-22 14:52:08 +0200

    EX19

---
+++ EX19.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int i;
+    vector <int> tableau(5,3);
+    tableau.push_back(8);
+    tableau.push_back(7);
+    tableau.push_back(6);
+    tableau.pop_back();
+    tableau.pop_back();
+    for (i=0; i<8; i++)
+    {
+        cout << tableau[i]<< endl;
+    }
+    cout << "Le tableau contient " << tableau.size()<<"cases." << endl;
+    return 0;
+}
