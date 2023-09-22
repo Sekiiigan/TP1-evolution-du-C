fb78097bb431f55c3a2c4b49bf3daecd8b7597d4
Sekiiigan <vidal.kerian@lyceejulesfil.fr>
2023-09-22 15:11:38 +0200

    EX24

---
+++ EX24.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int nombre_secondes(int minutes,int secondes,int heures=0);
+int main ()
+{
+    cout << nombre_secondes(10,20)<<endl;
+    return 0;
+}
+
+int nombre_secondes(int heures,int minutes,int secondes)
+{
+    int total=0;
+    total = heures *60 *60;
+    total += minutes *60;
+    total += secondes;
+    return total;
+}
